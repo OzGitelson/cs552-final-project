acb38a2afcfe3751cbcf93514495f46fa6caa4db
Oz Gitelson <oz.gitelson@yale.edu>
2025-05-05 20:06:14 -0500

    more cleanup

--- display_fluent_responses.py
+++ display_fluent_responses.py
@@ -9,34 +9,37 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from tqdm import tqdm
 
+from compare_results import load_results, choose_condition
 
-with open('results.json', 'r') as f:
-    data = json.load(f)
+def get_most_fluent(idx, data, num_responses=10):
 
-data=data[7][1]
-min_perplexity=float('inf')
-best_response=None
+    data=data[idx][1]
 
-ppl_model_name = "gpt2"
-device = "cuda" if torch.cuda.is_available() else "cpu"
-tokenizer = AutoTokenizer.from_pretrained(ppl_model_name)
-model = AutoModelForCausalLM.from_pretrained(ppl_model_name).to(device)
-model.eval()
+    ppl_model_name = "gpt2"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    tokenizer = AutoTokenizer.from_pretrained(ppl_model_name)
+    model = AutoModelForCausalLM.from_pretrained(ppl_model_name).to(device)
+    model.eval()
 
-perplexities = []
-with torch.no_grad():
-    for text in tqdm(data):
-        input_ids = tokenizer(text, return_tensors="pt").input_ids.to(device)
-        labels = input_ids.clone()
-        outputs = model(input_ids, labels=labels)
-        neg_log_likelihood = outputs.loss.item() * (input_ids.size(1) - 1)
-        perplexity= math.exp(neg_log_likelihood / (input_ids.size(1) - 1))
+    perplexities = []
+    with torch.no_grad():
+        for text in tqdm(data):
+            input_ids = tokenizer(text, return_tensors="pt").input_ids.to(device)
+            labels = input_ids.clone()
+            outputs = model(input_ids, labels=labels)
+            neg_log_likelihood = outputs.loss.item() * (input_ids.size(1) - 1)
+            perplexity= math.exp(neg_log_likelihood / (input_ids.size(1) - 1))
 
-        perplexities.append(perplexity)
+            perplexities.append(perplexity)
 
 
-sorted_data = [x for _, x in sorted(zip(perplexities, data))][:10]
+    sorted_data = [x for _, x in sorted(zip(perplexities, data))][-num_responses:]
+    print(f"\nMost fluent responses (lowest perplexity):")
+    for i, text in enumerate(sorted_data):
+        print(f"Response {i+1}:")
+        print(text, '\n')
 
-for i, text in enumerate(sorted_data):
-    print(f"Response {i+1}:")
-    print(text)
+if __name__ == "__main__":
+    results=load_results()
+    idx = choose_condition(results)
+    get_most_fluent(idx, results)
